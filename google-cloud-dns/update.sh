2762e8e3605002a133e4734141e223e684fb8803
Dan Lien <dan@danclien.com>
2017-01-30 15:50:36 -0600

    Add comments to DNS zone files

--- google-cloud-dns/update.sh
+++ google-cloud-dns/update.sh
@@ -6,3 +6,4 @@ zone_file=$zone_name
 echo "Updating zone, ${zone_name}, with zone file, ${zone_file}..."
 
 gcloud dns record-sets import "${zone_file}" --zone-file-format --zone=${zone_name}
+# --delete-all-existing
